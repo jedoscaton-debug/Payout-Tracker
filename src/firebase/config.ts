7253472a32cba76a6efce0c4abd423ccf1ba16c5
Jedo Caton <jedo.s.caton@gmail.com>
2026-03-06 11:32:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-891966930-64bbd",
+  "appId": "1:729116465776:web:cefc038ae3158c5b09f2b5",
+  "apiKey": "AIzaSyBacUSyDbPQGBn91NgqiP-q45dIcHA84t4",
+  "authDomain": "studio-891966930-64bbd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "729116465776"
+};
